4d58c4f041d783b11395ccc5c1a1a41528b340c4
watcol <kantaa058@gmail.com>
2020-11-14 15:55:57 +0900

    Add crate "thiserror"

--- src/error.rs
+++ src/error.rs
@@ -1,44 +1,23 @@
+extern crate thiserror;
+
 /// The General Result Type
 pub type LinkResult<T> = Result<T, LinkError>;
 
-use std::error::Error;
-use std::fmt;
+use thiserror::Error;
 use std::io;
 use std::process::Output;
 
 /// The Linking Error
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum LinkError {
+    #[error("Command exited with the failure: {0:?}")]
     Cmd(Output),
-    IO(io::Error),
+    #[error("IO Error: {0}")]
+    IO(#[from] io::Error),
 }
 
 impl From<Output> for LinkError {
     fn from(o: Output) -> Self {
-        LinkError::Cmd(o)
-    }
-}
-
-impl From<io::Error> for LinkError {
-    fn from(e: io::Error) -> Self {
-        LinkError::IO(e)
-    }
-}
-
-impl fmt::Display for LinkError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Self::IO(e) => write!(f, "{}", e),
-            Self::Cmd(o) => write!(f, "command exied with code \"{}\"", o.status),
-        }
-    }
-}
-
-impl Error for LinkError {
-    fn source(&self) -> Option<&(dyn Error + 'static)> {
-        match self {
-            Self::IO(e) => Some(e),
-            Self::Cmd(_) => None,
-        }
+        Self::Cmd(o)
     }
 }
