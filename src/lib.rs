d1ccd4f889a09e584b13270a204da3300f70aec9
watcol <kantaa058@gmail.com>
2020-12-11 21:38:13 +0900

    Updated the ducument.

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,6 @@
 //! This crate provides general interface to integrate many linkers
 //! overflowing in the world (such as [Gnu ld](https://sourceware.org/binutils/),
 //! [LLD](https://lld.llvm.org/), etc.) and use as *one* rust library.
-//!
-//! See also [genlink-impl](https://github.com/watcol/genlink-impl) for concrete
-//! implementations for the popular linkers.
-//! (They are also good models to implement by yourself. ^^)
 
 mod error;
 mod genlink;
